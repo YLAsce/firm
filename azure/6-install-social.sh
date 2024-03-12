47931fe0f6f49b262881a33d3f0f3b72c96dadec
MA Yuqiang <ylasce@gmail.com>
2024-03-12 15:42:34 +0100

    up

--- azure/6-install-social.sh
+++ azure/6-install-social.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # bug in sed command: sed: 1: "../benchmarks/1-social- ...": invalid command code . need to run manual
+apt install -y docker-compose
+pip3 install asyncio aiohttp
+apt install -y libssl-dev
+apt install -y libz-dev
+apt install -y luarocks
+luarocks install luasocket
+
 KUBE_DNS_CLUSTER_IP=$(kubectl get service -n kube-system kube-dns -o yaml | grep "clusterIP:" | awk '{print $2}')
 echo ${KUBE_DNS_CLUSTER_IP}
 
-#sed -i "s/10.0.0.10/${KUBE_DNS_CLUSTER_IP}/g"  ../benchmarks/1-social-network/nginx-web-server/conf/nginx-k8s.conf
-#sed -i "s/10.0.0.10/${KUBE_DNS_CLUSTER_IP}/g"  ../benchmarks/1-social-network/media-frontend/conf/nginx-k8s.conf
+sed -i "s/10.0.0.10/${KUBE_DNS_CLUSTER_IP}/g"  ../benchmarks/1-social-network/nginx-web-server/conf/nginx-k8s.conf
+sed -i "s/10.0.0.10/${KUBE_DNS_CLUSTER_IP}/g"  ../benchmarks/1-social-network/media-frontend/conf/nginx-k8s.conf
 #Here the firm should be installed at /home/azureuser dir
 
 kubectl apply -f ../benchmarks/1-social-network/k8s-yaml/social-network-ns.yaml
