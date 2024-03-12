34b1f1e61a327e6682da6db10f26a51f177c2872
MA Yuqiang <ylasce@gmail.com>
2024-03-12 14:56:26 +0100

    up

--- azure/2-deploy-tracing-agent.sh
+++ azure/2-deploy-tracing-agent.sh
@@ -1,6 +1,6 @@
 
 export NAMESPACE='monitoring'
-kubectl create -f manifests/setup
+kubectl create -f ../manifests/setup
 kubectl create namespace observability
 kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.28.0/deploy/crds/jaegertracing.io_jaegers_crd.yaml
 kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.28.0/deploy/service_account.yaml
@@ -9,4 +9,4 @@ kubectl create -n observability -f https://raw.githubusercontent.com/jaegertraci
 kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.28.0/deploy/operator.yaml
 kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.28.0/deploy/cluster_role.yaml
 kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/v1.28.0/deploy/cluster_role_binding.yaml
-kubectl create -f manifests/
+kubectl create -f ../manifests/
--- azure/3-deploy-trance-grapher.sh
+++ azure/3-deploy-trance-grapher.sh
@@ -1,8 +1,11 @@
-az aks mesh enable --resource-group ryax_ops --name mlintra --revision asm-1-18
-az aks show --resource-group ryax_ops --name mlintra  --query 'serviceMeshProfile.mode'   # "Istio"
+# az aks mesh enable --resource-group ryax_ops --name mlintra --revision asm-1-18
+# az aks show --resource-group ryax_ops --name mlintra  --query 'serviceMeshProfile.mode'   # "Istio"
+kubectl create namespace kafka
+kubectl create -f strimzi-0.39.0/install/cluster-operator/ -n kafka
 cd ../trace-grapher
 # docker-compose run stack-builder
 # now a shell pops as root in the project directory of the stack-builder container
 cd deploy-trace-grapher
 make prepare-trace-grapher-namespace
 make install-components
+kubectl get svc -n trace-grapher
